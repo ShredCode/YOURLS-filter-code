16550ced77ad802cfe365f66fcac62bfff48bd15
ShredCode <shredcode@outlook.com>
2014-08-27 22:42:03 -0500

    defined constants for common variables

--- plugin.php
+++ plugin.php
@@ -9,12 +9,14 @@ Author: ShredCode <http://www.shredcode.com>
 Author URI: https://www.github.com/ShredCode 
 */
 
-yourls_add_action('activated_' . basename(__DIR__) . '/plugin.php', 'shred_code_create_code_table');
+DEFINE("PLUGIN_NAME", basename(__DIR__));
+DEFINE("SHRED_CODE_TABLE_NAME", 'shred_code_filter_code');
+yourls_add_action('activated_' . PLUGIN_NAME . '/plugin.php', 'shred_code_create_code_table');
 
 //create table if not created
 function shred_code_create_code_table($args) {
     global $ydb;
-    $sql = 'CREATE TABLE IF NOT EXISTS ' . 'shred_code_filter_code' . ' (keyword VARCHAR(200) NOT NULL, PRIMARY KEY(keyword),code VARCHAR(3));';
+    $sql = 'CREATE TABLE IF NOT EXISTS ' . SHRED_CODE_TABLE_NAME . ' (keyword VARCHAR(200) NOT NULL, PRIMARY KEY(keyword),code VARCHAR(3));';
     $ydb->query($sql);
 }
 
@@ -25,7 +27,7 @@ function shred_code_delete_status_code($args) {
     global $ydb;
     $keyword = $args[0];
     $keyword = yourls_escape(yourls_sanitize_string($keyword));
-    $delete  = $ydb->query("DELETE FROM shred_code_filter_code WHERE keyword = '$keyword';");
+    $delete  = $ydb->query("DELETE FROM ". SHRED_CODE_TABLE_NAME ."WHERE keyword = '$keyword';");
 }
 
 yourls_add_filter('redirect_code', 'shred_code_find_status_code');
@@ -38,7 +40,7 @@ function shred_code_find_status_code($code, $location) {
     $url_components = parse_url($location);
     if (!empty($url_components['query']))
         $location = rtrim($location, '?');
-    $status_code = $ydb->get_var("SELECT shred_code_filter_code.code FROM shred_code_filter_code,yourls_url WHERE yourls_url.keyword = shred_code_filter_code.keyword AND yourls_url.url='$location';");
+    $status_code = $ydb->get_var("SELECT sc.code FROM ". SHRED_CODE_TABLE_NAME ." AS sc,yourls_url WHERE yourls_url.keyword = sc.keyword AND yourls_url.url='$location';");
     
     if (!$status_code) {
         $status_code = '301';
@@ -51,7 +53,7 @@ function shred_code_find_status_code($code, $location) {
 yourls_add_action('plugins_loaded', 'shred_code_status_add_page');
 
 function shred_code_status_add_page() {
-    yourls_register_plugin_page(basename(__DIR__), 'Status Code Page', 'shred_code_status_code_do_page');
+    yourls_register_plugin_page(PLUGIN_NAME, 'Status Code Page', 'shred_code_status_code_do_page');
 }
 
 
@@ -60,14 +62,14 @@ function shred_code_status_code_do_page() {
     // Check if a form was submitted
     if (isset($_POST['input_code']) && isset($_POST['input_keyword'])) {
         // Check nonce
-        yourls_verify_nonce(basename(__DIR__));
+        yourls_verify_nonce( PLUGIN_NAME );
         
         // Process form
         shred_code_page_update_code();
     }
     
     // Create nonce
-    $nonce = yourls_create_nonce(basename(__DIR__));
+    $nonce = yourls_create_nonce( PLUGIN_NAME );
     
     echo <<<HTML
 		<h2>Status Code Plugin Administration Page</h2>
@@ -77,7 +79,7 @@ function shred_code_status_code_do_page() {
 HTML;
     
     global $ydb;
-    $codes_results = $ydb->get_results("SELECT keyword,code FROM shred_code_filter_code ORDER BY keyword;");
+    $codes_results = $ydb->get_results("SELECT keyword,code FROM ". SHRED_CODE_TABLE_NAME ." ORDER BY keyword;");
     if ($codes_results) {
         echo "<table><th>Keyword</th><th>Status Code</th>";
         foreach ($codes_results as $code_result) {
@@ -114,7 +116,7 @@ HTML;
         $dropdown .= '<option value=' . $option . '>' . $option . '</option>' . "\n";
     }
     $dropdown .= '</select>' . "\n";
-    echo "$dropdown";
+    echo $dropdown;
     echo '<input type="hidden" name="nonce" value=' . $nonce . ' />';
     echo '<p><input type="submit" value="Update Status Code" /></p>';
     echo "</form>";
@@ -133,6 +135,6 @@ function shred_code_page_update_code() {
         
         global $ydb;
         // Update value in database
-        $ydb->query("INSERT into shred_code_filter_code (keyword,code) VALUES('$keyword','$code') ON DUPLICATE KEY UPDATE code='$code';");
+        $ydb->query("INSERT into ". SHRED_CODE_TABLE_NAME ." (keyword,code) VALUES('$keyword','$code') ON DUPLICATE KEY UPDATE code='$code';");
     }
 }
